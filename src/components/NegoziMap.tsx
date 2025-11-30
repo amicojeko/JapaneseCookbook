27bc9e771f3d14b650f80a4a95de8740319cd789
jeko <jeko@jeko.net>
2025-11-30 19:28:04 +0100

    update map style

--- src/components/NegoziMap.tsx
+++ src/components/NegoziMap.tsx
@@ -28,8 +28,8 @@ const NegoziMap: React.FC = () => {
             scrollWheelZoom={true}
           >
             <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://carto.com/">CartoDB</a>'
+              url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
             />
 
             {NEGOZI.map((shop) => (
